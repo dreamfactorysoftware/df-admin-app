d4df00c46f04a74726c964bfb4ba547b5c43332f
Todd Appleton <toddappleton@dreamfactory.com>
2015-09-15 13:38:29 -0400

    Schema field validation JSON editor.

--- app/admin_components/adf-schema/dreamfactory-schema.js
+++ app/admin_components/adf-schema/dreamfactory-schema.js
@@ -1436,5 +1436,83 @@ angular.module('dfSchema', ['ngRoute', 'dfUtility'])
                 }
             }
         }
-    }]);
+    }])
+
+    .directive('jsonEdit', function () {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            template: '<textarea ng-model="jsonEditing"></textarea>',
+            replace: true,
+            scope: {
+                model: '=jsonEdit'
+            },
+            link: function (scope, element, attrs, ngModelCtrl) {
+
+                function setEditing(value) {
+                    scope.jsonEditing = angular.copy(JSON2String(value));
+                }
+
+                function updateModel(value) {
+                    scope.model = string2JSON(value);
+                }
+
+                function setValid() {
+                    ngModelCtrl.$setValidity('json', true);
+                }
+
+                function setInvalid() {
+                    ngModelCtrl.$setValidity('json', false);
+                }
+
+                function string2JSON(text) {
+                    try {
+                        return angular.fromJson(text);
+                    } catch (err) {
+                        setInvalid();
+                        return text;
+                    }
+                }
+
+                function JSON2String(object) {
+                    // better than JSON.stringify(), because it formats + filters $$hashKey etc.
+                    // NOTE that this will remove all $-prefixed values
+                    return angular.toJson(object, true);
+                }
+
+                function isValidJson(model) {
+                    var flag = true;
+                    try {
+                        angular.fromJson(model);
+                    } catch (err) {
+                        flag = false;
+                    }
+                    return flag;
+                }
+
+                //init
+                setEditing(scope.model);
+
+                //check for changes going out
+                scope.$watch('jsonEditing', function (newval, oldval) {
+                    if (newval != oldval) {
+                        if (isValidJson(newval)) {
+                            setValid();
+                            updateModel(newval);
+                        } else {
+                            setInvalid();
+                        }
+                    }
+                }, true);
+
+                //check for changes coming in
+                scope.$watch('model', function (newval, oldval) {
+                    if (newval != oldval) {
+                        setEditing(newval);
+                    }
+                }, true);
+
+            }
+        };
+    });
 
