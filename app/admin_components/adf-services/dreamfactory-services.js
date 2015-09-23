3ba5ecf7bdd30faca6b6ddd21b129ba0e65ec400
javascriptboss99 <javascriptboss99@gmail.com>
2015-09-23 11:56:06 -0500

    fix delete button in service config, rws. fixes #df-384

--- app/admin_components/adf-services/dreamfactory-services.js
+++ app/admin_components/adf-services/dreamfactory-services.js
@@ -1312,7 +1312,7 @@ angular.module('dfServices', ['ngRoute', 'dfUtility', 'dfServiceTemplates', 'dfS
                 }
 
                 scope.deleteObjectFromArray = function (arr, index) {
-                    arr.splice(index, 1);
+                    arr = arr.splice(index, 1);
                 };
 
                 scope.decorateSchema = function () {
