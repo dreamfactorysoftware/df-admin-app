4dab6acb21abb0b0638cf66bf5cad29a8183e75f
javascriptboss99 <javascriptboss99@gmail.com>
2016-01-21 14:59:00 -0600

    ace editor update

--- app/admin_components/adf-services/dreamfactory-services.js
+++ app/admin_components/adf-services/dreamfactory-services.js
@@ -165,6 +165,8 @@ angular.module('dfServices', ['ngRoute', 'dfUtility', 'dfServiceTemplates', 'dfS
                             serviceData.config[key] = arr;
                           }
                         });
+
+
                     }
 
                     return {
@@ -1335,6 +1337,9 @@ angular.module('dfServices', ['ngRoute', 'dfUtility', 'dfServiceTemplates', 'dfS
                     scope.selectedSchema.config_schema.forEach(function (schema) {
                         if (schema.default) {
                             scope.serviceInfo.record.config[schema.name] = scope.serviceInfo.record.config[schema.name] || schema.default;
+
+                        } else if(schema.name === "content" && scope.selectedSchema.group === "Custom") {
+                            scope.serviceInfo.record.config["content"] = scope.serviceInfo.record.config["content"] || "";
                         }
                     });
                 };
