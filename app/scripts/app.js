b02b913b473d2f28f960bc5c803337ae7f156496
Lee Hicks <leehicks@dreamfactory.com>
2016-07-10 22:06:11 -0400

    version bump and grunt build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,7 +40,7 @@ angular
     ])
 
     // Set application version number
-    .constant('APP_VERSION', '2.2.2')
+    .constant('APP_VERSION', '2.2.3')
 
     // Set global url for this application
     .constant('INSTANCE_URL', '')
