62562aad94ce40ede7140c07a18da0f978fef67a
Carsten Jacobsen <carstenjacobsen_dreamfactory@Carstens-MacBook-Pro.local>
2016-10-04 12:01:04 -0700

    Release 2.4.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,7 +40,7 @@ angular
     ])
 
     // Set application version number
-    .constant('APP_VERSION', '2.3.0')
+    .constant('APP_VERSION', '2.4.0')
 
     // Set global url for this application
     .constant('INSTANCE_URL', '')
