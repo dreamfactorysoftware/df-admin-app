76b1c7b5d18d18c87cc5cf7d1385612ef558f263
Lee Hicks <leehicks@dreamfactory.com>
2017-04-21 20:38:54 -0400

    version update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,7 +41,7 @@ angular
     ])
 
     // Set application version number
-    .constant('APP_VERSION', '2.6.0')
+    .constant('APP_VERSION', '2.7.0')
 
     // Set global url for this application
     .constant('INSTANCE_URL', '')
