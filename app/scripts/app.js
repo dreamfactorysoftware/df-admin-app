96172dcfb241333f96d4c64cb12d242cced893b0
Todd Appleton <toddappleton@dreamfactory.com>
2017-07-27 14:12:22 -0400

    Release 2.9.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,7 +41,7 @@ angular
     ])
 
     // Set application version number
-    .constant('APP_VERSION', '2.8.1')
+    .constant('APP_VERSION', '2.9.0')
 
     // Set global url for this application
     .constant('INSTANCE_URL', '')
