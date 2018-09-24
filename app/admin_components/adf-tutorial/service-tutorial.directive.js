f4336a37a4ea2ab3412841340c1bdb49ab63ab20
Volodymyr Tymtsiv <volodymyrt@interlink-ua.com>
2018-09-24 16:06:04 +0300

    Extract method to dfTutorial service.

--- app/admin_components/adf-tutorial/service-tutorial.directive.js
+++ app/admin_components/adf-tutorial/service-tutorial.directive.js
@@ -2,7 +2,7 @@
 
 angular.module('dfTutorial')
 
-    .directive('dfServiceTutorial', ['MOD_TUTORIALS_ASSET_PATH', '$location', function (MOD_TUTORIALS_ASSET_PATH, $location) {
+    .directive('dfServiceTutorial', ['MOD_TUTORIALS_ASSET_PATH', 'dfTutorial', function (MOD_TUTORIALS_ASSET_PATH, dfTutorial) {
 
         return {
             restrict: 'E',
@@ -19,87 +19,10 @@ angular.module('dfTutorial')
                         }
                     });
 
-                    allScenarios.createService.tour = tour;
+                    dfTutorial.fillWithScenario(tour, allScenarios.createService);
 
-                    allScenarios.createService.steps
-                        .forEach(function (step) {
-                            var createdStep = tour.addStep(step.name, step.options);
-
-                            bindByType(step, createdStep);
-
-                            step.customFunctionObjects.forEach(function (each) {
-                                createdStep.on(each.event, each.handler);
-                            })
-                        });
                     tour.start();
                 };
-
-                function getSelector(step) {
-                    return step.options.attachTo.element;
-                }
-
-                function bindHighlighting(step) {
-                    var selector = getSelector(step);
-
-                    step.on('show', function () {
-                        angular.element(selector).addClass('highlighted-element');
-                    });
-                    step.on('before-hide', function () {
-                        angular.element(selector).removeClass('highlighted-element')
-                    });
-                }
-
-                function bindEmptyInputsValidation(step) {
-                    var inputSelector = getSelector(step);
-                    var buttonSelector = '.shepherd-element .shepherd-content footer .shepherd-buttons li:last-child .shepherd-button';
-                    var disabledButtonClass = 'tutorial-disabled-button';
-
-                    step.on('show', function () {
-                        if ($(inputSelector).val() == '') {
-                            $(buttonSelector).addClass(disabledButtonClass);
-                        }
-
-                        $(inputSelector).on('input', function (e) {
-                            if ($(inputSelector).val() == '') {
-                                $(buttonSelector).addClass(disabledButtonClass)
-                            } else {
-                                $(buttonSelector).removeClass(disabledButtonClass)
-                            }
-                        });
-
-                    });
-
-                    step.on('before-hide', function () {
-                        $(buttonSelector).removeClass("tutorial-disabled-button")
-                    });
-                }
-
-                function bindFocus(step) {
-                    var selector = getSelector(step);
-                    step.on('show', function () {
-                        $(selector).focus();
-                    });
-                }
-
-                function bindByType(step, createdStep) {
-                    switch (step.type) {
-                        case 'click': {
-                            bindHighlighting(createdStep);
-                            break;
-                        }
-                        case 'input': {
-                            bindEmptyInputsValidation(createdStep);
-                            bindFocus(createdStep);
-                            bindHighlighting(createdStep);
-                            break;
-                        }
-                        case 'select': {
-                            bindHighlighting(createdStep);
-                        }
-                    }
-                }
-
-
             }
         };
     }]);
