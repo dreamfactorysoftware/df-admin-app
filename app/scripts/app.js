a9cdd3c3ff844ff81167803c0125a6390927c921
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2020-06-30 18:04:36 +0300

    Bump APP_VERSION version to 4.5.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -120,7 +120,7 @@ angular
     })
 
     // Set application version number
-    .constant('APP_VERSION', '4.4.0')
+    .constant('APP_VERSION', '4.5.0')
 
     // Set global url for this application
     .constant('INSTANCE_BASE_URL', '')
