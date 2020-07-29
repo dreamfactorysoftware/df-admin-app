cc0108eb3748f959f9ae826a0f222b0669409855
Szymon Juchno <szymon.juchno@uxpin.com>
2020-07-29 17:09:49 +0200

    Set proper url for search, improve styles

--- app/admin_components/adf-scripts/dreamfactory-scripts.js
+++ app/admin_components/adf-scripts/dreamfactory-scripts.js
@@ -60,7 +60,7 @@ angular.module('dfScripts', ['ngRoute', 'dfUtility'])
             };
 
             $scope.searchForScripts = function() {
-              $window.open('https://www.google.com/search?q=' + $scope.search.text, '_blank');
+              $window.open('https://dreamfactory.com/scripts/search/' + $scope.search.text, '_blank');
             };
 
             // Loosely defined script object for when a script is non-existent.
