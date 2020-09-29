0331bd5d7578e17ed75ba0178592988138d69386
Jason Gilmore <wj@wjgilmore.com>
2020-09-29 13:38:03 -0400

    Release 4.3.2
    * DP-224 Understand how the demo applications are installed
    
    - Build the dist
    
    * Bump APP_VERSION version to 4.5.0
    
    * DP-228. Improve paywall page design and contact references (#212)
    
    * DP-228 Create custom paywall pages for restricted services
    * Bumped df-admin-app version declaration to 4.5.1
    
    * DP-228 (#213)
    
    * draft for flexible paywall content (service-specific marketing messages)
    
    * Bump APP_VERSION version to 4.5.0
    
    * DP-228 Create custom paywall pages for restricted services
    
    * Additional improvements
    
    * Removed dev port
    
    * Finished paywall redesign
    
    * Bumped df-admin-app version declaration to 4.5.1
    
    * Added GA parameters
    
    Co-authored-by: Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -120,7 +120,7 @@ angular
     })
 
     // Set application version number
-    .constant('APP_VERSION', '4.5.0')
+    .constant('APP_VERSION', '4.5.1')
 
     // Set global url for this application
     .constant('INSTANCE_BASE_URL', '')
