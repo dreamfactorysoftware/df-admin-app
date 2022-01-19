7c24e60975e2cd94f7e528d8432ffbae0980fc38
Tomo Norman <tomo.l.norman@gmail.com>
2022-01-19 19:45:31 +0900

    Release 4.10.1

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -120,7 +120,7 @@ angular
     })
 
     // Set application version number
-    .constant('APP_VERSION', '4.10.0')
+    .constant('APP_VERSION', '4.10.1')
 
     // Set global url for this application
     .constant('INSTANCE_BASE_URL', '')
