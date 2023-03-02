e458765171b35eadc30e1df9e55fa3ac9734b26a
Danil Lytvyn <daniilly@softwareplanetgroup.com>
2023-03-02 12:26:46 +0200

    Release 5.0.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -120,7 +120,7 @@ angular
     })
 
     // Set application version number
-    .constant('APP_VERSION', '4.14.1')
+    .constant('APP_VERSION', '5.0.0')
 
     // Set global url for this application
     .constant('INSTANCE_BASE_URL', '')
